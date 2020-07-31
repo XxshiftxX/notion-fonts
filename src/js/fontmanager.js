991563115a8d951a71736692ae2b170be425c957
XxshiftxX <contact@dev-shift.me>
2020-07-31 14:39:34 +0900

    폰트 적용 방식 개선

--- src/js/fontmanager.js
+++ src/js/fontmanager.js
@@ -1,42 +1,75 @@
 const changeFont = (selector, font) => {
-  $(selector).css('font-family', font);
-  $(selector).find('div').each(function () {
-    const style = $(this).attr('style');
-    if (!style || style.includes(font)) {
-      return;
-    }
+    const root = $(selector);
+    changeDOMFont(root, font);
+    
+    root.find('div').each(function () {
+
+        changeDOMFont(this, font);
+    })
+};
+
+const changeDOMFont = (element, font) => {
+    const style = $(element).attr('style');
     const fontFamilyRegex = /font-family: ([^;]*);/;
-    if (!style.match(fontFamilyRegex)) {
-      return;
+
+    if (!style || style.includes(font) || !style.match(fontFamilyRegex)) {
+        return;
     }
-    const applied = style.replace(fontFamilyRegex, `font-family: ${font}, $1`);
-    $(this).attr('style', applied);
-    $(this).change(function () { $(this).attr('style', applied); });
-  })
-}
+    
+    const isCodeblock = $(element).parent().hasClass('notion-code-block');
+    if (isCodeblock) font = 'd2coding';
+
+    const applied = style.replace(fontFamilyRegex, `font-family: ${font}, $1;`);
+    $(element).attr('style', applied);
+};
 
 const addWebFont = (name, url) => {
-  const font = new FontFace(name, `url(${url})`);
-  font.load();
-  document.fonts.add(font);
+    if (document.fonts.check(`12px ${name}`)) return;
+
+    const font = new FontFace(name, `url(${url})`);
+    font.load();
+    document.fonts.add(font);
 }
 
 whale.runtime.onMessage.addListener((req, sender, res) => {
-  addWebFont('Noto Sans', '//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin.woff');
-  addWebFont('Cafe24', 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Oneprettynight.woff');
-  addWebFont('GGB', 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/GyeonggiBatang.woff');
-  addWebFont('MPLUS', 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap');
+    addWebFont('Noto Sans', '//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin.woff');
+    addWebFont('Cafe24', 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Oneprettynight.woff');
+    addWebFont('GGB', 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/GyeonggiBatang.woff');
+    addWebFont('MPLUS', 'https://fonts.gstatic.com/s/mplusrounded1c/v10/VdGEAYIAV6gnpUpoWwNkYvrugw9RuPWDpq8A_4TPv30lLyDK_FIhT0K-oTdprA.16.woff2');
 
+    const fonts = 'GGB, MPLUS, D2Coding'
 
-  let checkExist = setInterval(() => {
-    if ($('.notion-page-content').length) {
-      changeFont('.notion-page-content', 'GGB, MPLUS, D2Coding');
-      changeFont('.notion-page-block', 'GGB, MPLUS, D2Coding');
-      changeFont('.notion-collection_view_page-block', 'GGB, MPLUS, D2Coding');
-      clearInterval(checkExist);
-    }
-    else if ($('.notion-collection_view_page-block').length) {
-      changeFont('.notion-collection_view_page-block', 'GGB, MPLUS, D2Coding');
-    }
-  }, 100);
+    document.addEventListener('DOMNodeInserted', (event) => {
+        event.path.forEach(element => changeDOMFont(element, fonts));
+    });
+
+    let checkExist = setInterval(() => {
+        if ($('.notion-page-content').length) {
+            changeFont('.notion-page-content', fonts);
+            changeFont('.notion-page-block', fonts);
+            changeFont('.notion-list-view', fonts);
+            changeFont('.notion-collection_view_page-block', fonts);
+            clearInterval(checkExist);
+        }
+        else if ($('.notion-list-view').length) {
+            changeFont('.notion-page-content', fonts);
+            changeFont('.notion-page-block', fonts);
+            changeFont('.notion-list-view', fonts);
+            changeFont('.notion-collection_view_page-block', fonts);
+        }
+        else if ($('.notion-app-inner').length) {
+            changeFont('.notion-page-content', fonts);
+            changeFont('.notion-page-block', fonts);
+            changeFont('.notion-list-view', fonts);
+            changeFont('.notion-collection_view_page-block', fonts);
+        }
+        else if ($('.notion-collection_view_page-block').length) {
+            changeFont('.notion-page-content', fonts);
+            changeFont('.notion-page-block', fonts);
+            changeFont('.notion-list-view', fonts);
+            changeFont('.notion-collection_view_page-block', fonts);
+        }
+    }, 100);
+
+    res({});
 });
